e2365a8329acf70e39f835f6ccf1f1d9904d8761
Mario Cordoba <mariocordoba96@gmail.com>
2022-08-25 10:08:11 -0600

    agregado

--- SCP_/Form1.cs
+++ SCP_/Form1.cs
@@ -20,7 +20,7 @@ namespace SCP_
 
         private void button2_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("TEST");
+           // MessageBox.Show("TEST");
             this.Close();
         }
 
