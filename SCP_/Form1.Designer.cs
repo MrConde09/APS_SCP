c2ef989669d12a3b08558e441b38e9eb36a3543c
Mario Cordoba <mariocordoba96@gmail.com>
2022-08-25 09:36:58 -0600

    Subiendo otros cambios

--- SCP_/Form1.Designer.cs
+++ SCP_/Form1.Designer.cs
@@ -29,33 +29,59 @@ namespace SCP_
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(75, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(627, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BIENVENIDO AL MENU PRINCIPAL";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(310, 116);
+            this.button1.Location = new System.Drawing.Point(142, 280);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ACEPTAR";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(426, 280);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 31);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "SALIR";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
